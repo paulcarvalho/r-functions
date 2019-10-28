e9634001921e14444f7f2716daa5286b9a934028
paulcarvalho <paulcarvalho@uri.edu>
2019-10-28 16:41:11 -0400

    cleaned up code

--- check_spelling.R
+++ check_spelling.R
@@ -1,89 +1,93 @@
-### AUTHOR: Paul Carvalho    #####
-### Date created: 03/15/2018 #####
+################################################################################################################
+# 
+# AUTHOR: Paul Carvalho    
+# DATE CREATED: 03/15/2018
+# DATE MODIFIED: 10/28/2019 
+#
+# DESCRIPTION: Check for similar spelling within, or between, character or factor vectors and run through user 
+#              interaction to make spelling corrections.              
+#
+# INPUTS:  
+#   df_1: dataframe containing character or factor vector to check spelling.
+#   var_1: bare parameter name (ie, without quotes) indicating the vector to check spelling.
+#   df_2: second dataframe with additional character vector (optional).
+#   var_2: bare parameter name (ie, without quotes) indicating the vector to add to the first vector (optional).
+#   distance_sensitivity: positive integer indicating approximate string distance between characters in char
+#                         vector (see ?adist for more details). Values 1 to 3 should be sufficient to catch  
+#                         spelling mistakes.
+#
+# OUTPUTS: updated dataframe with spelling corrections.
+#
+################################################################################################################
 
-# DESCRIPTION: Check for similar spelling within character or factor vector and run through user interaction 
-#              to make changes
-# INPUTS:  df_in                = dataframe containing character or factor vector to check spelling.
-#          var_name             = bare parameter name indicating the vector to check spelling.
-#          df_in_1              = second data frame with additional character vector.
-#          var_name_1           = bare parameter name indicating the vector to add to the first vector.
-#          distance_sensitivity = positive integer indicating approximate string distance between characters in char
-#                                 list. 2-3 should be sufficient to catch spelling mistakes.
-# OUTPUTS: updated dataframe
-
-# Libraries ______________________________________________________________________________________________________
+# Libraries
+install.packages("rlang")
+install.packages("dplyr")
 library(rlang)
 library(dplyr)
 
-# Check spelling of vector _______________________________________________________________________________________
-check.spelling <- function(df_in_1, var_name_1, df_in_2=NULL, var_name_2=NULL, distance_sensitivity){
-  # turn bare args into quosures
-  quo_var <- enquo(var_name_1)
-  if(is.null(df_in_2) == FALSE){
-    quo_var_1 <- enquo(var_name_2)  
+# Function
+check.spelling <- function(df_1, var_1, df_2 = NULL, var_2 = NULL, distance_sensitivity){
+  
+  # turn bare inputs into quosures
+  quo_var <- enquo(var_1)
+  if(is.null(df_2) == FALSE){
+    quo_var_1 <- enquo(var_2)  
   }
   
   # quo_text(quo_var) gets the string of the column name from bare parameter input
-  char.list <- as.character(df_in_1[[quo_text(quo_var)]])
-  if(is.null(df_in_2) == FALSE){
-    char.list.1 <- as.character(df_in_2[[quo_text(quo_var_1)]])
+  char.list <- as.character(df_1[[quo_text(quo_var)]])
+  if(is.null(df_2) == FALSE){
+    char.list.1 <- as.character(df_2[[quo_text(quo_var_1)]])
   }
 
   # Remove na's from the list of characters
   char.list <- na.omit(char.list)
-  if(is.null(df_in_2) == FALSE){
+  if(is.null(df_2) == FALSE){
     char.list.1 <- na.omit(char.list.1)
   }
 
-  # check spelling and obtain a list of words that have similar spelling
+  # check spelling and create a list of words that have similar spelling
   x <- unique(char.list)
-  df_list <- data_frame(x=x, name=rep("df_1",length(x)))
-  # Only run this code if there is a second dataframe
-  if(is.null(df_in_2) == FALSE){
+  df_list <- data_frame(x = x, name=rep("df_1",length(x)))
+  
+  # If there is a second dataframe, add unique spelling to dataframe
+  if(is.null(df_2) == FALSE){
     y <- unique(char.list.1)
-    df_x <- data_frame(x=x, name=rep("df_1",length(x)))
-    df_y <- data_frame(x=y, name=rep("df_2",length(y)))
+    df_x <- data_frame(x = x, name = rep("df_1",length(x)))
+    df_y <- data_frame(x = y, name = rep("df_2",length(y)))
     df_list <- rbind(df_x,df_y)
   }
   
-  # Need to run this nested for loop to get the length of the data frame
+  # Need to run this nested for loop to get the length of the dataframe
   tmp_count <- 0
   for(i in 1:length(df_list$x)){
     for(j in i:length(df_list$x)){
-      dist = adist(df_list$x[i],df_list$x[j])
+      dist <- adist(df_list$x[i], df_list$x[j])
       if(dist > 0 && dist <= distance_sensitivity){
-        tmp_count <- tmp_count+1
-      }
-    }
-  }
-  df_dist = data.frame(name_1=character(length=tmp_count),
-                       df_name_1=character(length=tmp_count),
-                       name_2=character(length=tmp_count),
-                       df_name_2=character(length=tmp_count),
-                       distance=numeric(length=tmp_count))
-  # need to make sure these vectors are characters and not factors
-  df_dist$name_1 <- as.character(df_dist$name_1)
-  df_dist$df_name_1 <- as.character(df_dist$df_name_1)
-  df_dist$name_2 <- as.character(df_dist$name_2)
-  df_dist$df_name_2 <- as.character(df_dist$df_name_2)
+        tmp_count <- tmp_count + 1
+  }}}
+  
+  # create an empty dataframe shell of the correct length
+  df_dist <- data.frame(name_1 = character(length=tmp_count),
+                        df_name_1 = character(length=tmp_count),
+                        name_2 = character(length=tmp_count),
+                        df_name_2 = character(length=tmp_count),
+                        distance = numeric(length=tmp_count))
 
-  count = 0
+  # create data frame with list of similar spelling
+  count <- 0
   for(i in 1:length(df_list$x)){
     for(j in i:length(df_list$x)){
-      dist = adist(df_list$x[i],df_list$x[j])
+      dist <- adist(df_list$x[i], df_list$x[j])
       if(dist > 0 && dist <= distance_sensitivity){
-        count <- count+1
-        # print(df_dist$name_1[count])
-        # print(df_list$x[i])
-        df_dist$name_1[count]    <- df_list$x[i]
-        df_dist$name_2[count]    <- df_list$x[j]
+        count <- count + 1
+        df_dist$name_1[count] <- df_list$x[i]
+        df_dist$name_2[count] <- df_list$x[j]
         df_dist$df_name_1[count] <- df_list$name[i]
         df_dist$df_name_2[count] <- df_list$name[j]
-        df_dist$distance[count]  <- dist
-        
-      }
-    }
-  }
+        df_dist$distance[count] <- dist
+  }}}
 
   # create a new list that omits repeated pairs
   tmp_name_1 <- NULL
@@ -91,43 +95,37 @@ check.spelling <- function(df_in_1, var_name_1, df_in_2=NULL, var_name_2=NULL, d
   tmp_name_2 <- NULL
   tmp_df_name_2 <- NULL
   tmp_distance <- NULL
-  # refined_df_dist <- data.frame(name_1=character(),
-  #                               df_name_1=character(),
-  #                               name_2=character(),
-  #                               df_name_2=character(),
-  #                               distance=numeric())
-
   
-  count.1 = 0
+  # iterate through the dataframe and remove repeated pairs
+  count_1 <- 0
   for(k in 1:length(df_dist$name_1)){
     if(k == 1){
-      count.1 <- count.1+1
-      tmp_name_1[count.1]    <- df_dist$name_1[k]
-      tmp_name_2[count.1]    <- df_dist$name_2[k]
-      tmp_df_name_1[count.1] <- df_dist$df_name_1[k]
-      tmp_df_name_2[count.1] <- df_dist$df_name_2[k]
-      tmp_distance[count.1]  <- df_dist$distance[k]
-
+      count_1 <- count_1 + 1
+      tmp_name_1[count_1] <- df_dist$name_1[k]
+      tmp_name_2[count_1] <- df_dist$name_2[k]
+      tmp_df_name_1[count_1] <- df_dist$df_name_1[k]
+      tmp_df_name_2[count_1] <- df_dist$df_name_2[k]
+      tmp_distance[count_1] <- df_dist$distance[k]
     } else {
-      flag = 0
+      flag <-  0
       for(m in 1:length(tmp_name_1)){
         if(df_dist$name_1[k] == tmp_name_1[m] && df_dist$name_2[k] == tmp_name_2[m]){
-          flag = 1
+          flag <- 1
         } else if (df_dist$name_1[k] == tmp_name_2[m] && df_dist$name_2[k] == tmp_name_1[m]){
-          flag = 1
+          flag <- 1
         }
       }
       if(flag == 0){
-        count.1 = count.1+1
-        tmp_name_1[count.1]    <- df_dist$name_1[k]
-        tmp_name_2[count.1]    <- df_dist$name_2[k]
-        tmp_df_name_1[count.1] <- df_dist$df_name_1[k]
-        tmp_df_name_2[count.1] <- df_dist$df_name_2[k]
-        tmp_distance[count.1]  <- df_dist$distance[k]
+        count_1 <- count_1 + 1
+        tmp_name_1[count_1] <- df_dist$name_1[k]
+        tmp_name_2[count_1] <- df_dist$name_2[k]
+        tmp_df_name_1[count_1] <- df_dist$df_name_1[k]
+        tmp_df_name_2[count_1] <- df_dist$df_name_2[k]
+        tmp_distance[count_1] <- df_dist$distance[k]
 
-      }
-    }
-  }
+  }}}
+  
+  # create a refined list without repeated pairs in similar spelling
   refined_df_dist <- data.frame(name_1=tmp_name_1,
                                 df_name_1=tmp_df_name_1,
                                 name_2=tmp_name_2,
@@ -156,29 +154,27 @@ check.spelling <- function(df_in_1, var_name_1, df_in_2=NULL, var_name_2=NULL, d
       if(val==1){
         correct.sp    <- refined_df_dist$name_1[i]
         incorrect.sp1 <- refined_df_dist$name_2[i]
-        # substitute spelling and adjust factor levels - no changes will be made to df_in_2
-        df_in_1[[quo_text(quo_var)]] <- gsub(incorrect.sp1, correct.sp, df_in_1[[quo_text(quo_var)]])
+        # substitute spelling and adjust factor levels - no changes will be made to df_2
+        df_1[[quo_text(quo_var)]] <- gsub(incorrect.sp1, correct.sp, df_1[[quo_text(quo_var)]])
       } else if(val==2) {
         correct.sp    <- refined_df_dist$name_2[i]
         incorrect.sp1 <- refined_df_dist$name_1[i]
         # substitute spelling and adjust factor levels
-        df_in_1[[quo_text(quo_var)]] <- gsub(incorrect.sp1, correct.sp, df_in_1[[quo_text(quo_var)]])
+        df_1[[quo_text(quo_var)]] <- gsub(incorrect.sp1, correct.sp, df_1[[quo_text(quo_var)]])
       } else if(val==3){
         correct.sp <- readline(prompt="Enter correct spelling: ")
         incorrect.sp1 <- refined_df_dist$name_1[i]
         incorrect.sp2 <- refined_df_dist$name_2[i]
         # substitute spelling and adjust factor levels
-        df_in_1[[quo_text(quo_var)]] <- gsub(incorrect.sp1, correct.sp, df_in_1[[quo_text(quo_var)]])
-        df_in_1[[quo_text(quo_var)]] <- gsub(incorrect.sp2, correct.sp, df_in_1[[quo_text(quo_var)]])
+        df_1[[quo_text(quo_var)]] <- gsub(incorrect.sp1, correct.sp, df_1[[quo_text(quo_var)]])
+        df_1[[quo_text(quo_var)]] <- gsub(incorrect.sp2, correct.sp, df_1[[quo_text(quo_var)]])
       } else if(val==4){
         # do nothing
       } else {
         cat("\nInvalid option")
-      }
-    } # End of interactive for loop
-  }
+  }}}
 
-  return(df_in_1)
+  return(df_1)
 }
 
 
